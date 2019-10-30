3ed6bbddaebc1fa6812190117e54f2ad4bc76857
deadoggy <yinjiazhang7@gmail.com>
2019-10-30 13:48:43 +0200

    test done

---
+++ src/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+from centroid_index import _label_to_list
+from centroid_index import _sum_orphan
+from centroid_index import _center_as_prototype
+from centroid_index import centroid_index
+
+def test_label_to_list():
+
